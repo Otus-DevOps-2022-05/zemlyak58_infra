2cd12c6a739c81d4566022b8b2d30088fc80e918
Sergey Ilyushin <zemlyak58@gmail.com>
2022-07-17 18:02:14 +0300

    test

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -21,6 +21,7 @@ module "vpc" {
 module "app" {
   source          = "../modules/app"
   public_key_path = var.public_key_path
+  private_key_path = var.private_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = module.vpc.app_subnet_id
 }
@@ -28,6 +29,7 @@ module "app" {
 module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
+  private_key_path = var.private_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = module.vpc.app_subnet_id
 }
